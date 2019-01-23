1885f8ca6f92e69d096476c7a940197dd860697b
Johanna Lee <johanna.lee@rangle.io>
2019-01-23 18:29:25 -0500

    Update search term with store

--- src/app/components/card-list.component.ts
+++ src/app/components/card-list.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Robot } from '../store/robot.type';
+import { SearchStore } from '../store/search/search.store';
 
 @Component({
   selector: 'app-card-list',
   template: `
-    <div *ngFor="let robot of robots">
+    <div *ngFor="let robot of filterRobots()">
       <app-card [robot]="robot" ></app-card>
     </div>
   `,
 })
 
-export class CardListComponent {
+export class CardListComponent implements OnDestroy {
   public robots: Robot[] = [
     {id: 1, name:'John', email: 'john@staplescanada.ca'},
     {id: 2, name:'Sherry', email: 'sherry@staplescanada.ca'},
     {id: 3, name:'Fred', email: 'fred@staplescanada.ca'},
   ];
 
-  constructor() {}
+  private subscription: any;
+  private searchTerm = '';
+
+  constructor(private searchStore: SearchStore) {
+    this.subscription = searchStore.getSearchTerm().subscribe(state => {
+      this.searchTerm = state.toLowerCase();
+    });
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  public filterRobots(): Robot[] {
+    return this.robots.filter(robot => robot.name.toLowerCase().includes(this.searchTerm));
+  }
 }
